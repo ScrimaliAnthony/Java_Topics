c939b685e3d4ad31431fcc8932d1e734e6ccf1de
Scrimali Anthony <anthony.scrimali@gmail.com>
2024-12-27 17:43:55 +0100

    One-line condition with ternary operator

---
+++ One-line condition with ternary operator/Categorize integer as positive, negative, or zero/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Read the integer input
+        int num = scanner.nextInt();
+
+        // Use a ternary operator to check if the integer is positive, negative or zero
+        // and print the corresponding message
+        String resultat = (num > 0) ? "Positive"
+                            : (num < 0) ? "Negative"
+                            : "Zero";
+
+        System.out.print(resultat);
+
+    }
+}
