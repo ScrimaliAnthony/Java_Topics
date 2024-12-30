3adc55f1864f07ff8906413cd3f55c4bf0376754
Scrimali Anthony <anthony.scrimali@gmail.com>
2024-12-30 10:36:26 +0100

    Array

--- Array/Basic operations/src/Task.java
+++ Array/Basic operations/src/Task.java
@@ -2,9 +2,6 @@
 
 public class Task {
   public static void main(String[] args) {
-    int arr[] = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    int n = 6;
-    n = arr[arr[n]];
-    System.out.print(n);
+    // put your code here
   }
 }
