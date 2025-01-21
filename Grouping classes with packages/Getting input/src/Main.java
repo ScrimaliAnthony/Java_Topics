cdbea05904c7c7b5c89146fa6564bc7ea563c24f
Scrimali Anthony <anthony.scrimali@gmail.com>
2025-01-21 19:15:05 +0100

    Grouping classes with packages

---
+++ Grouping classes with packages/Getting input/src/Main.java
@@ -0,0 +1,8 @@
+class InputReader {
+    public static String getString() {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        String input = scanner.nextLine();
+        scanner.close();
+        return input;
+    }
+}
